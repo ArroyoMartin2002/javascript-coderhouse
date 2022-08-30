3960dfeb60c5f6787586c45ace9bbf6e88dbf202
Martin Arroyo <arroyomartin2002@gmail.com>
2022-08-30 14:53:17 -0300

    1er Desafio Complementario

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let numero = parseInt(prompt("ingrese un numero:  (escriba 000 para salir)"))
+
+while(numero !=000) { 
+    let numero2 = parseInt(prompt("   +   "))
+    let suma = numero + numero2
+    console.log(suma)
+    alert("El resultado de " + numero + " + " + numero2 + " es igual a " + suma )
+}
+
